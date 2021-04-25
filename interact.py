4001bd26fa53ec9edc4016079287e2d83e4f31dc
whitecas18 <whitecas18@students.ecu.edu>
2021-04-25 17:24:56 -0400

    add interact class

---
+++ interact.py
@@ -0,0 +1,19 @@
+class interact:
+	def __init__(self, text, answer=None, obj=None):
+		# self.name = name
+		self.text = text
+		self.obj = obj
+		self.answer = answer
+
+
+	# def getName(self):
+	# 	return self.name
+
+	def getText(self):
+		return self.text
+
+	def getAnswer(self):
+		return self.answer
+
+	def getObj(self):
+		return self.obj
